88fe2b9ff55ea3f11d9505af9e19312365cace61
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2025-12-23 23:43:10 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import To_Do_List from './To_Do_List/To_Do_List'
+
+const App = () => {
+  return (
+    <div>
+      <To_Do_List />
+    </div>
+  )
+}
+
+export default App
